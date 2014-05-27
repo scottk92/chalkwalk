8901cf4248dc21f2b8884045b13155eb166c4395
Joanna Kim <joannakim13@gmail.com>
2014-05-27 03:21:43 -0700

    adding create_results w/content

---
+++ public/javascripts/create_results.js
@@ -0,0 +1,18 @@
+
+
+var createResultsPage = function(coordHash, colorHash) {
+	//create canvas
+	var image = createDrawing();
+	
+//iterate through hash : get key: color: array of arrays
+	var nameList = Object.keys(coordHash);
+	for (var i = 0; i < nameList.length; i++) {
+		var player = nameList[i];
+		var color = colorHash[player];
+		var lines = coordHash[player];
+		for (var numLines = 0; numLines < lines.length; numLines++) {
+			var line = lines[numLines];
+			drawPoints(color, image, line);
+		}
+	}
+}
