abff465ffa8cb02bfc710bbbdf58d7304a38fc2c
Joanna Kim <joannakim13@gmail.com>
2014-06-09 22:13:34 -0700

    poss fixedffix

---
+++ public/javascripts/nav-bar.js
@@ -0,0 +1,16 @@
+function navBarFix(classN) {
+/* we need this only on touch devices */
+if (Modernizr.touch) {
+    /* cache dom references */ 
+    var $body = jQuery('body'); 
+
+    /* bind events */
+    $(document)
+    .on('focus', 'input', function(e) {
+        $body.addClass(classN);
+    })
+    .on('blur', 'input', function(e) {
+        $body.removeClass(classN);
+    });
+} 
+}
