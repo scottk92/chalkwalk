aa80094596b55ba5c6b05f9e3d3cb42a5495abd3
Joanna Kim <joannakim13@gmail.com>
2014-06-10 09:36:06 -0700

    back

--- public/javascripts/panel-transition.js
+++ public/javascripts/panel-transition.js
@@ -7,13 +7,17 @@ var panels = [namePanel, searchPanel, createPanel];
 
 // Show the panel of the id given.  Hide the others.
 function showPanel(panelId) {
+	alert("yo");
   for (var i=0; i<panels.length; i++) {
+  	
     if (i == panelId) {
 		panels[i].style.display = 'block';
-		if (panelId == 1 || panelId == 2) {
+		/*if (panelId == 1 || panelId == 2) {
 			document.getElementById("nameBtn").style.display = 'block';
+		}*/
     } else {
 		panels[i].style.display = 'none';
-		document.getElementById("nameBtn").style.display = 'none';
+		//document.getElementById("nameBtn").style.display = 'none';
 	}
+}
 }
