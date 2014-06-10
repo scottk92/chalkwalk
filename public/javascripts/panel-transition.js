a151c7451efe9fcd34da499c0fa9f07616923815
Joanna Kim <joannakim13@gmail.com>
2014-06-10 10:36:24 -0700

    btn effects

--- public/javascripts/panel-transition.js
+++ public/javascripts/panel-transition.js
@@ -7,17 +7,16 @@ var panels = [namePanel, searchPanel, createPanel];
 
 // Show the panel of the id given.  Hide the others.
 function showPanel(panelId) {
-	alert("yo");
   for (var i=0; i<panels.length; i++) {
-  	
     if (i == panelId) {
 		panels[i].style.display = 'block';
-		/*if (panelId == 1 || panelId == 2) {
+		if (panelId == 1 || panelId == 2) {
 			document.getElementById("nameBtn").style.display = 'block';
-		}*/
+		} else {
+			document.getElementById("nameBtn").style.display = "none";
+		}
     } else {
 		panels[i].style.display = 'none';
-		//document.getElementById("nameBtn").style.display = 'none';
 	}
 }
 }
